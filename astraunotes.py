165ffb44cc8bcaee37f53fa4d246e78292b0e0d6
Jeanne0410 <jeanne.leroux@etudiant.univ-rennes2.fr>
2024-02-15 10:27:37 +0100

    Liste des astronautes dans l'iss

--- astraunotes.py
+++ astraunotes.py
@@ -2,4 +2,7 @@ import requests
 
 contenu = requests.get("http://api.open-notify.org/astros.json")
 for personne in contenu.json()["people"]:
-    print(personne)
+    if personne['craft']=='ISS':
+        print(personne)
+
+#les info author et date sont stocké a chaque commit 
