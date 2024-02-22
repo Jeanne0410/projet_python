7f386e0ef714cd8da27cea813ab97a6fdab4575e
Jeanne0410 <jeanne.leroux@etudiant.univ-rennes2.fr>
2024-02-22 09:52:24 +0100

    nom prenom

--- astraunotes.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-contenu = requests.get("http://api.open-notify.org/astros.json")
-for personne in contenu.json()["people"]:
-    if personne['craft']=='ISS':
-        print(personne)
-
-#les info author et date sont stocké a chaque commit 
